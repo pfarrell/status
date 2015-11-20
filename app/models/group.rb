b4e0d26584f72c37a7e87c6580979cfddb0f1cff
Patrick Farrell <mr.patfarrell@gmail.com>
2015-11-19 18:44:45 -0800

    hash of hash of arrays on pivot

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,11 +3,12 @@ class Group < Sequel::Model
 
   def pivot
     require 'byebug'
-    h = Hash.new{|h,k| h[k] = []}
+    h = Hash.new{|h,k| h[k] = Hash.new{|h,k| h[k] = []}}
     self.entries.each do |entry| 
+      entry_key = h[entry.name]
       entry.statuses.each do |status|
         status.value.each do |k,v| 
-          h[k] << v
+          entry_key[k] << v
         end
       end
     end
