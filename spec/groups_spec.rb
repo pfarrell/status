408065553ba97aaf129606c0b5a2eeb99b47aa9d
Patrick Farrell <mr.patfarrell@gmail.com>
2015-12-17 08:03:03 -0800

    group tests

--- spec/groups_spec.rb
+++ spec/groups_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe 'App' do
+  let(:group) { Group.find_or_create(name: "test_group") }
+
   it "should allow access to the groups page" do
     get "/groups"
     expect(last_response).to be_ok
@@ -9,6 +11,16 @@ describe 'App' do
 
   it "should return json for groups page" do
     get "/groups.json"
+    expect(last_response).to be_ok
+  end
+
+  it "should return latest statuses" do
+    get "/groups/#{group.name}/latest"
+    expect(last_response).to be_ok
+  end
+
+  it "should return latest statuses in json format" do
+    get "/groups/#{group.name}/latest.json"
     expect(last_response).to be_ok
   end
 end
