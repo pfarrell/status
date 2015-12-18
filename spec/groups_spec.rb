3a513087f6352167a75673b18767d6450517a83c
Patrick Farrell <mr.patfarrell@gmail.com>
2015-12-18 08:50:14 -0800

    tests for group

--- spec/groups_spec.rb
+++ spec/groups_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
+require 'json'
 
 describe 'App' do
   let(:group) { Group.find_or_create(name: "test_group") }
+
+  let(:status) { Status.new(group: group, value: {"test_key"=>"test_val"}.to_json) } 
 
   it "should allow access to the groups page" do
     get "/groups"
@@ -21,6 +24,22 @@ describe 'App' do
 
   it "should return latest statuses in json format" do
     get "/groups/#{group.name}/latest.json"
+    expect(last_response).to be_ok
+  end
+
+  it "should return all statuses for a group" do
+    status.save
+    get "/groups/#{group.name}"
+    expect(last_response).to be_ok
+  end
+  
+  it "should return all statuses for a group in json format" do
+    get "/groups/#{group.name}.json"
+    expect(last_response).to be_ok
+  end
+
+  it "allows you to post statuses at it" do
+    post "/groups", {statuses: [{name: "#{group.name}", value: {"test_key"=> "test_val"}}]}.to_json
     expect(last_response).to be_ok
   end
 end
