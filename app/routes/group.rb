1585e347925eb98e40404860b1e09146295d3214
Patrick Farrell <mr.patfarrell@gmail.com>
2015-12-18 08:50:02 -0800

    refactor to remove entries

--- app/routes/group.rb
+++ app/routes/group.rb
@@ -39,11 +39,10 @@ class App < Sinatra::Application
   post "/groups" do
     content_type :json
     ret=[]
-    group = Group.find_or_create(group: params[:group])
     obj = JSON.parse(request.body.read)
     obj["statuses"].each do |status|
-      entry = Entry.find_or_create(group: group, name: status["name"])
-      status = Status.new(entry: entry, value: status)
+      group = Group.find_or_create(name: status["name"])
+      status = Status.new(group: group, value: status["value"])
       status.save
       ret << status
     end
