86c6f8bd8d229b4a3481268389fe2fb803711655
Patrick Farrell <mr.patfarrell@gmail.com>
2016-01-04 23:20:40 -0800

    rename app to status

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -4,6 +4,6 @@ describe 'App' do
   it "should allow access to the home page" do
     get "/"
     expect(last_response).to be_ok
-    expect(last_response.body).to match(/App/)
+    expect(last_response.body) .to match(/Status/)
   end
 end
